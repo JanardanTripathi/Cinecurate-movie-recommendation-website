aa779e1e639e2f5b58602c065a747908805c0631
JanardanTripathi <janardant47@gmail.com>
2024-11-11 23:25:28 +0530

    modify context.js

--- src/Components/context.js
+++ src/Components/context.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { createContext, useState, useEffect, useContext } from "react";
 
 const AppContext = createContext();
